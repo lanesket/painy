8506130f8b3c1a05bb140df1dd9a61740d333d3f
Raevskiy Rudolf <gamesrud@yandex.ru>
2023-03-22 12:56:19 +0100

    Version updated from 0.1.2 to 0.1.3 in setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="Painy",
-    version="0.1.2",
+    version="0.1.3",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
