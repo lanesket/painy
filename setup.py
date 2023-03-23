b9de20677c84244fb7b31b51df29133ef60656cc
Raevskiy Rudolf <gamesrud@yandex.ru>
2023-03-23 17:44:36 +0100

    Bumped version from 0.1.3 to 0.1.4. Added package data for 'p_extensions.txt' file.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="Painy",
-    version="0.1.3",
+    version="0.1.4",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,8 +21,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    
-    
+    include_package_data=True,
+    package_data={
+        'painy': ['p_extensions.txt']
+    },
     entry_points={
         'console_scripts': [
             "painy = painy.main:main"
