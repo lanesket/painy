35b9d8f917bea8d7bb8bf91097b0db36f4eb53df
Raevskiy Rudolf <gamesrud@yandex.ru>
2023-04-03 22:12:25 +0200

    Refactor Model's process_input_tokens method to use tiktoken for encoding and decoding input. Add tiktoken to requirements.txt.

--- painy/chat.py
+++ painy/chat.py
@@ -2,6 +2,7 @@ from .git import *
 from .enums import *
 import openai
 import os
+import tiktoken
 
 
 MODEL_NAME = os.getenv(key="OPENAI_MODEL_NAME", default="gpt-3.5-turbo")
@@ -27,13 +28,13 @@ class ChatHistory:
 class Model:
     def __init__(self, purpose_prompt: str, model_name: str = MODEL_NAME):
         self.model_name = model_name
-        self.purpose_prompt = purpose_prompt
+        self.purpose_prompt = Model.process_input_tokens(purpose_prompt, self.model_name)
         self.history = ChatHistory()
         
         self.history.add(self.purpose_prompt, ChatRole.SYSTEM)
         
     def get_response(self, prompt: str) -> str:
-        prompt = prompt[:MAX_LENGTH]
+        prompt = Model.process_input_tokens(prompt, self.model_name)
         self.history.add(prompt, ChatRole.USER)
         
         response = openai.ChatCompletion.create(
@@ -41,6 +42,14 @@ class Model:
             messages=self.history.get(),
         )
         
+        usage = response['usage']
         response = response["choices"][0]["message"]["content"]
         
         return response
+    
+    @staticmethod
+    def process_input_tokens(input: str, model_name: str) -> int:
+        encoding = tiktoken.encoding_for_model(model_name)
+        encoded = encoding.encode(input)
+        decoded = encoding.decode(encoded)[:MAX_LENGTH]
+        return decoded
